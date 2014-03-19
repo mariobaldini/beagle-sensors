328adf2cc19138fb58eb9f853b90b8c1f5ac5746
Mario Baldini <mario.baldini@gmail.com>
2014-03-19 16:47:05 -0300

    adx 4 plots ok 5- FPS

--- src/v5_graphics/main2.py
+++ src/v5_graphics/main2.py
@@ -7,20 +7,43 @@ import pyqtgraph as pg
 import random
 from pyqtgraph.ptime import time
 
-#QtGui.QApplication.setGraphicsSystem('raster')
+from pyqtgraph import MultiPlotWidget
+
 app = QtGui.QApplication([])
+w = pg.GraphicsWindow()
+w.setWindowTitle('Beaglebone Health Sensors Experiments')
+p1 = w.addPlot(0,0, title="Accelerometer - I2C-1 0x1D  (G)")
+p2 = w.addPlot(1,0, title="Accelerometer - I2C-1 0x53  (G)")
+p3 = w.addPlot(2,0, title="Accelerometer - I2C-2 0x1D  (G)")
+p4 = w.addPlot(3,0, title="Accelerometer - I2C-2 0x1D  (G)")
+
+p1.showGrid(True, True)
+p2.showGrid(True, True)
+p3.showGrid(True, True)
+p4.showGrid(True, True)
+w.show()
+
+
+
+
+
+#QtGui.QApplication.setGraphicsSystem('raster')
+##app = QtGui.QApplication([])
 #mw = QtGui.QMainWindow()
 #mw.resize(800,800)
 
-p = pg.plot()
-p.setWindowTitle('pyqtgraph example: MultiPlotSpeedTest')
 
 data_window_size = 500
 
-p.setYRange(-1, 1)
-p.setXRange(0, data_window_size)
+
+##p = pg.plot()
+##p.setWindowTitle('pyqtgraph example: MultiPlotSpeedTest')
+
+
+# p.setYRange(-1, 1)
+p1.setXRange(0, data_window_size)
 # p.resize(600,600)
-p.enableAutoScale()
+p1.enableAutoScale()
 
 # p.clear()
 # p.plot([0,1,-1], pen='r')
@@ -41,9 +64,9 @@ cZ = pg.PlotCurveItem(pen='b')
 cZ.setPos(0,1)
 
 
-p.addItem(cX)
-p.addItem(cY)
-p.addItem(cZ)
+p1.addItem(cX)
+p1.addItem(cY)
+p1.addItem(cZ)
 
 
 lastTime = time()
@@ -69,7 +92,10 @@ def update():
   dt = now - lastTime
   lastTime = now
   fps = 1.0/dt
-  p.setTitle('%0.2f fps' % fps)
+  # p1.setTitle('%0.2f fps' % fps)
+  # w.setWindowTitle('%0.2f fps' % fps)
+  
+  print fps
 
 
 
