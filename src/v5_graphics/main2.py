d451536b84b9b2c9c3d381f0eb3531bfe934a901
Mario Baldini <mario.baldini@gmail.com>
2014-03-19 16:26:55 -0300

    triple line chart ok FPS 50

--- src/v5_graphics/main2.py
+++ src/v5_graphics/main2.py
@@ -15,35 +15,53 @@ app = QtGui.QApplication([])
 p = pg.plot()
 p.setWindowTitle('pyqtgraph example: MultiPlotSpeedTest')
 
-nSamples = 500
+data_window_size = 500
 
-p.setYRange(-3, 3)
-p.setXRange(0, nSamples)
-p.resize(600,600)
+p.setYRange(-1, 1)
+p.setXRange(0, data_window_size)
+# p.resize(600,600)
+p.enableAutoScale()
 
-p.clear()
-p.plot([0,1,-1], pen='r')
+# p.clear()
+# p.plot([0,1,-1], pen='r')
 
 
 
 
-data = [random.uniform(-2.0, 2.0) for x in range(10000)]
+dataX = [random.uniform(-2.0, 2.0) for x in range(10000)]
+dataY = [random.uniform(-2.0, 2.0) for x in range(10000)]
+dataZ = [random.uniform(-2.0, 2.0) for x in range(10000)]
 
-data_window = data[len(data)-100:len(data)] 
-print data_window
+
+cX = pg.PlotCurveItem(pen='r')
+cX.setPos(0,1)
+cY = pg.PlotCurveItem(pen='g')
+cY.setPos(0,1)
+cZ = pg.PlotCurveItem(pen='b')
+cZ.setPos(0,1)
+
+
+p.addItem(cX)
+p.addItem(cY)
+p.addItem(cZ)
 
 
 lastTime = time()
 def update():
-  global data
-  
-  # data.append(random.uniform(-2.0, 2.0))
-  # data_window = data[len(data)-100:len(data)] 
-  # p.plot(data_window, pen='r')
+  global dataX, dataY, dataZ, data_window_size
+
+  dataX.append(random.uniform(-2.0, 2.0))
+  dataY.append(random.uniform(-2.0, 2.0))
+  dataZ.append(random.uniform(-2.0, 2.0))
+
+
+  cX.setData(dataX[len(dataX)-data_window_size:len(dataX)])
+  cY.setData(dataY[len(dataY)-data_window_size:len(dataY)])
+  cZ.setData(dataZ[len(dataZ)-data_window_size:len(dataZ)])
   
 
-  p.addItem(pg.PlotCurveItem([1,2,1,-1], pen='b'))
-
+  #p.addItem(pg.PlotCurveItem([1,2,1,-1], pen='b'))
+  
 
 
   global lastTime
